ab6b1ac3032a11c8c7d99b4c6f27b22f5c66027a
powsokan <powsi1997@gmail.com>
2020-05-02 15:58:44 +0530

    image download end pont

--- src/main/java/com/powsikan/TextDetector_Backend/pictures/PictureController.java
+++ src/main/java/com/powsikan/TextDetector_Backend/pictures/PictureController.java
@@ -2,12 +2,16 @@ package com.powsikan.TextDetector_Backend.pictures;
 
 
 import lombok.SneakyThrows;
+import lombok.extern.flogger.Flogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -23,6 +27,7 @@ public class PictureController {
     public List<Picture> getImages() {
         return pictureService.getImages();
     }
+
     @GetMapping("/picture/{id}")
     public Optional<Picture> getImage(Integer id) {
         return pictureService.getImage(id);
@@ -35,8 +40,37 @@ public class PictureController {
 
     @SneakyThrows
     @GetMapping("/uploads/{filename}")
-    public Resource getImage(@PathVariable String filename) {
-        return pictureService.loadFileAsResource(filename);
+    public ResponseEntity<Resource> getImageResource(@PathVariable String filename, HttpServletRequest request) {
+
+        Resource resource = pictureService.loadFileAsResource(filename);
+        // Try to determine file's content type
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+
+        // Fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
     @PostMapping("picture/{username}")
