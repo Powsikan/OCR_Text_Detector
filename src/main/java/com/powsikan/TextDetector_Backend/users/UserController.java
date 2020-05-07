973844676aee09d7f648099c3584becc027ac4a6
powsokan <powsi1997@gmail.com>
2020-05-07 08:32:43 +0530

    image download end pont

--- src/main/java/com/powsikan/TextDetector_Backend/users/UserController.java
+++ src/main/java/com/powsikan/TextDetector_Backend/users/UserController.java
@@ -2,6 +2,7 @@ package com.powsikan.TextDetector_Backend.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
